a1e93f15ecf4293e5599599e9813ec7065462016
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-04-19 15:53:06 +0900

    서블릿 - 1단계 테스트 실습

---
+++ src/test/java/study/code/Item.java
@@ -0,0 +1,26 @@
+package study.code;
+
+public class Item {
+
+    private long id;
+    private long price;
+    private String description;
+
+    public Item(long id, long price, String description) {
+        this.id = id;
+        this.price = price;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/test/java/study/code/User.java
@@ -0,0 +1,20 @@
+package study.code;
+
+public class User {
+
+    private final long id;
+    private final Pay pay;
+
+    public User(long id, Pay pay) {
+        this.id = id;
+        this.pay = pay;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Pay getPay() {
+        return pay;
+    }
+}
