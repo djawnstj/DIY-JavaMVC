ca9d6943605ef33566f8ce8ed5f19b5aba2fc174
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-05-08 17:24:18 +0900

    리플렉션 실습 2 - 로컬 push

--- src/main/java/com/djawnstj/mvcframework/bean/BeanFactory.java
+++ src/main/java/com/djawnstj/mvcframework/bean/BeanFactory.java
@@ -3,7 +3,6 @@ package com.djawnstj.mvcframework.bean;
 import org.reflections.Reflections;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -19,13 +18,5 @@ public class BeanFactory {
                 .stream()
                 .filter(type -> !(type.isAnnotation() && type.isInterface()))
                 .collect(Collectors.toSet());
-    }
-
-    public Object getBean(Class<?> clazz) {
-        try {
-            return clazz.getDeclaredConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
--- src/test/java/study/reflection/ComponentScannerTest.java
+++ src/test/java/study/reflection/ComponentScannerTest.java
@@ -1,10 +1,9 @@
 package study.reflection;
 
-import com.djawnstj.mvcframework.annotation.Component;
-import com.djawnstj.mvcframework.bean.BeanFactory;
 import com.djawnstj.mvcframework.code.UserRepository;
 import com.djawnstj.mvcframework.code.UserService;
-import org.junit.jupiter.api.Assertions;
+import com.djawnstj.mvcframework.context.ApplicationContext;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -19,32 +18,25 @@ class ComponentScannerTest {
     @DisplayName("어노테이션 조회 테스트")
     void testAnnotation() {
         String packageName = "com.djawnstj.mvcframework";
-        BeanFactory beanFactory = new BeanFactory(packageName);
 
-        Set<Class<?>> classes = beanFactory.scanAnnotation(Component.class);
+        // given
+        ApplicationContext applicationContext = new ApplicationContext(packageName);
+        applicationContext.init();
 
-        boolean contains = classes.contains(UserRepository.class) && classes.contains(UserService.class);
-        Assertions.assertTrue(contains);
+        // when
+        Object bean1 = applicationContext.getBean(UserService.class);
+        Object bean2 = applicationContext.getBean(UserRepository.class);
+
+        // then - 검증
+        Assertions.assertThat(bean1).isNotNull();
+        Assertions.assertThat(bean2).isNotNull();
     }
 
     @Test
     @DisplayName("빈 등록 및 조회 테스트 - 모든 객체는 싱글톤 패턴으로 구현")
     void addBeanTest() {
         String packageName = "com.djawnstj.mvcframework";
-        BeanFactory beanFactory = new BeanFactory(packageName);
 
-        Set<Class<?>> classes = beanFactory.scanAnnotation(Component.class);
 
-        Set<Object> instances = new HashSet<>();
-
-        for (Class<?> clazz : classes) {
-            logger.debug(clazz.getName());
-            // 인스턴스 생성
-//            Object instance = beanFactory.getBean(clazz);
-//            instances.add(instance);
-        }
-
-        // 두 컬렉션의 사이즈가 같다면 객체도 하나만 생성된 것이기 때문에 싱글톤
-        Assertions.assertEquals(classes.size(),instances.size());
     }
 }
