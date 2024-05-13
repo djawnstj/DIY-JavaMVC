d8b806cf1d19ab1a161db7bb3ab089c987db4956
annnhs <hoseong1218@gmail.com>
2024-05-13 17:04:35 +0900

    [2nd week] 3단계 요구사항1 시작

--- src/test/java/com/djawnstj/mvcframework/bean/BeanFactoryTest.java
+++ src/test/java/com/djawnstj/mvcframework/bean/BeanFactoryTest.java
@@ -1,6 +1,5 @@
 package com.djawnstj.mvcframework.bean;
 
-import com.djawnstj.mvcframework.annotation.Component;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +18,17 @@ public class BeanFactoryTest {
         Object userService2 = beanFactory.getBean(UserService.class);
 
         assertThat(userService1).isSameAs(userService2);
+    }
+
+    @Test
+    @DisplayName("@Autowired 애너테이션이 붙은 생성자의 파라미터 주입")
+    void checkEqualsDependencyAndBean() {
+        final String basePackage = "com.djawnstj.mvcframework";
+        BeanFactory beanFactory = new BeanFactory(basePackage);
+        beanFactory.init();
+        UserRepository userRepository = (UserRepository) beanFactory.getBean(UserRepository.class);
+        UserService userService = (UserService) beanFactory.getBean(UserService.class);
+
     }
 
 }
--- src/test/java/com/djawnstj/mvcframework/bean/UserService.java
+++ src/test/java/com/djawnstj/mvcframework/bean/UserService.java
@@ -1,8 +1,28 @@
 package com.djawnstj.mvcframework.bean;
 
+import com.djawnstj.mvcframework.annotation.AutoWired;
 import com.djawnstj.mvcframework.annotation.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService() {
+        this.userRepository = new UserRepository();
+    }
+
+    @AutoWired
+    public UserService(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void register(final String id, final String pw) {
+        this.userRepository.save(id, new User(id, pw));
+    }
+
+    public List<User> getUsers() {
+        return (List<User>) this.userRepository.findAll();
+    }
 }
