c48be423e7509856cfd4da0f2cbc995e3b10fd29
s00nju <tnswn258@gmail.com>
2024-05-20 15:56:26 +0900

    피드백 반영

--- src/test/java/study/code/PayService.java
+++ src/test/java/study/code/PayService.java
@@ -7,19 +7,13 @@ public class PayService {
         this.userRepository = userRepository;
     }
 
-    /**
-     * 잔액 충전하기
-     */
     public long charge(long userId, int amount) {
         User user = userRepository.findUserById(userId);
         Pay pay = user.getPay();
-        pay.setBalance(pay.getBalance() + amount); // 충전
+        pay.setBalance(pay.getBalance() + amount);
         return pay.getBalance();
     }
 
-    /**
-     * 상품 구매하기
-     */
     public void buyProduct(long userId, Product product) {
         User user = userRepository.findUserById(userId);
         Pay pay = user.getPay();
@@ -28,8 +22,7 @@ public class PayService {
 
         if(balance < price) {
             throw new IllegalArgumentException("잔액이 충분하지 않습니다.");
-        } else {
-            pay.setBalance(balance - price); // 구매
         }
+        pay.setBalance(balance - price);
     }
 }
