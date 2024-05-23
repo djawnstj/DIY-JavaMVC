28e43e0128e2824846e5cf463569ea78f9eae0b9
s00nju <tnswn258@gmail.com>
2024-05-23 16:56:39 +0900

    요구사항 1단계 수정

--- src/test/java/study/reflection/ReflectionTest.java
+++ src/test/java/study/reflection/ReflectionTest.java
@@ -6,7 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import study.reflection.code.Car;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -15,10 +20,23 @@ public class ReflectionTest {
     @DisplayName("Car 객체 정보 가져오기")
     void showClass() {
         // 요구사항 1. Car 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력
+        //given
         Class<Car> carClass = Car.class;
-        logger.debug(carClass.getName());
-        logger.debug("Field : {}", Arrays.toString(carClass.getDeclaredFields()));
-        logger.debug("Method : {}", Arrays.toString(carClass.getDeclaredMethods()));
-        logger.debug("Constructor : {}", (Object) carClass.getDeclaredConstructors());
+
+        //when
+        String name = carClass.getName();
+        logger.debug(name);
+        Field[] fields = carClass.getDeclaredFields();
+        logger.debug("Field : {}", Arrays.toString(fields));
+        Method[] methods = carClass.getDeclaredMethods();
+        logger.debug("Method : {}", Arrays.toString(methods));
+        Constructor<?>[] constructors = carClass.getDeclaredConstructors();
+        logger.debug("Constructor : {}", (Object) constructors);
+
+        //then
+        assertThat(name).isEqualTo("Car");
+        assertThat(fields).isNotEmpty();
+        assertThat(methods).isNotEmpty();
+        assertThat(constructors).isNotEmpty();
     }
 }
